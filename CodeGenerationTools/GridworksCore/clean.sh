160804f82223ce0f9edff594aabd9b27997b95a4
Andrew Schweitzer <schweitz72@gmail.com>
2024-08-07 14:38:55 -0400

    Code generation changes to remove warnings

--- CodeGenerationTools/GridworksCore/clean.sh
+++ CodeGenerationTools/GridworksCore/clean.sh
@@ -1,14 +1,4 @@
-black ../../src/gwbase/enums/
-isort ../../src/gwbase/enums/
-
-black ../../src/gwbase/types/
-isort ../../src/gwbase/types/
-
-black ../../tests/enums/
-isort ../../tests/enums/
-
-black ../../tests/types/
-isort ../../tests/types/
-
-black ../../src/gwbase/types/__init__.py
-black ../../src/gwbase/enums/__init__.py
+pushd ../..
+ruff check --fix --select I
+# ruff check --fix
+# ruff format

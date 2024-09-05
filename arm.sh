ebec373ee6c4f418e7ca9b20f23e5021cdd1a0f9
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-09-05 11:13:35 -0400

    better GwStrEnum; docker-compose -> docker compose

--- arm.sh
+++ arm.sh
@@ -3,5 +3,5 @@
 # your computer has an arm architecture. Otherwise use x86.sh
 docker stop gw-dev-rabbit
 cp for_docker/arm.yml arm.yml
-docker-compose -f arm.yml up -d
+docker compose -f arm.yml up -d
 rm arm.yml
